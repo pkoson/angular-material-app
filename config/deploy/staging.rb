6da76746ade4a36c11acf10e759961ed3fd364ae
Tomasz Kulik <Tomasz.Kulik@akra.net>
2016-04-26 12:51:01 +0200

    capistrano deployment settings

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -6,10 +6,13 @@
 # server 'example.com', user: 'deploy', roles: %w{app db web}, my_property: :my_value
 # server 'example.com', user: 'deploy', roles: %w{app web}, other_property: :other_value
 # server 'db.example.com', user: 'deploy', roles: %w{db}
-server 'dev.akra.net', port: 5001, user: 'akrapeople2', roles: %w{web app db}
+server 'dev.akra.net', port: 5001, user: 'sportmatrix', roles: %w{web app db}
 
 set :rails_env, :production
 set :default_shell, "/bin/bash --login"
+
+# Default deploy_to directory is /var/www/my_app_name
+set :deploy_to, '/home/sportmatrix/deploy/staging'
 
 # role-based syntax
 # ==================
