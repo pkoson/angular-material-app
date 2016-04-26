e5ec80a4f051d314f6aff3a049fcfe382d8f25a3
Tomasz Kulik <Tomasz.Kulik@akra.net>
2016-04-26 15:59:49 +0200

    settings for staging

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -14,6 +14,23 @@ set :default_shell, "/bin/bash --login"
 # Default deploy_to directory is /var/www/my_app_name
 set :deploy_to, '/home/sportmatrix/deploy/staging'
 
+set :keep_releases, 5
+
+set :puma_rackup, -> { File.join(current_path, 'config.ru') }
+set :puma_state, "#{shared_path}/tmp/pids/puma.state"
+set :puma_pid, "#{shared_path}/tmp/pids/puma.pid"
+set :puma_bind, "unix://#{shared_path}/tmp/sockets/puma.sock"    #accept array for multi-bind
+set :puma_conf, "#{shared_path}/puma.rb"
+set :puma_access_log, "#{shared_path}/log/puma_error.log"
+set :puma_error_log, "#{shared_path}/log/puma_access.log"
+set :puma_role, :app
+set :puma_env, fetch(:rack_env, fetch(:rails_env, 'production'))
+# set :puma_threads, [0, 2]
+# set :puma_workers, 0
+set :puma_worker_timeout, nil
+set :puma_init_active_record, true
+set :puma_preload_app, false
+
 # role-based syntax
 # ==================
 
