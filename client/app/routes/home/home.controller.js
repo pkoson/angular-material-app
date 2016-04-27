2a07d9b3bcfa4ac9786ab85d48aed71fa8bb3437
Piotr Koson <pior.koson@akra.net>
2016-04-27 14:38:28 +0200

    Moves navbar from index.html to home module & adds rwd to navbar

--- client/app/routes/home/home.controller.js
+++ client/app/routes/home/home.controller.js
@@ -1,12 +1,17 @@
 export default class HomeController {
-  constructor($http) {
+  constructor($http, $mdSidenav) {
     this.$http = $http;
+    this.$mdSidenav = $mdSidenav;
     this.getUsers();
   }
 
   getUsers(){
     this.$http
       .get('http://api.lvh.me:3000/v1/users')
-      .then(response => this.users = response.data)
+      .then(response => this.users = response.data);
+  }
+  openLeftMenu() {
+      console.log('test open left');
+      this.$mdSidenav('left').toggle();
   }
 }
