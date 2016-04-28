9fc83ba7da6e9bcec45ada110615b0a561c6daae
Piotr Koson <pior.koson@akra.net>
2016-04-28 08:57:29 +0200

    Adds form validations & adds material design svg icons

--- client/app/config/ngTheme.js
+++ client/app/config/ngTheme.js
@@ -1,5 +1,7 @@
-export default function ngConfig($mdThemingProvider) {
+export default function ngConfig($mdThemingProvider, $mdIconProvider) {
   $mdThemingProvider.theme('default')
     .primaryPalette('blue')
     .accentPalette('orange');
+  $mdIconProvider
+    .defaultIconSet('../assets/images/mdi.svg');
 }
