fda9c8937d980b4edd7bc75cd5bb30256b9e492d
Bartlomiej Slipek <bslipek@gmail.com>
2016-04-29 12:16:53 +0200

    Updates API route in client application

--- client/app/config/interceptors/apiRequestInterceptor.js
+++ client/app/config/interceptors/apiRequestInterceptor.js
@@ -1,6 +1,6 @@
 export default function apiRequestInterceptor () {
   "ngInclude";
-  const BASE_API_PATH = 'http://api.lvh.me:3000/v1';
+  const BASE_API_PATH = 'http://lvh.me:3000/api/v1';
   
   return {
     request(config) {
