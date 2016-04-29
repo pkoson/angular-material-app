87c9151c5bae86a6d42d26922a62d807856d6b16
Jerzy Daszkowski <jerzy.daszkowski@akra.net>
2016-04-29 10:50:23 +0200

    add icon for translate switcher [#117892207]

--- client/app/components/translateSwitcher/translateSwitcher.directive.js
+++ client/app/components/translateSwitcher/translateSwitcher.directive.js
@@ -1,14 +1,20 @@
-export default function translateSwitcher() {
-  function translateSwitchCtrl($translate, $scope) {
-    $scope.changeLanguage = (lang) => {
+export default function translateSwitcher($translate, $rootScope) {
+  let translateSwitchCtrl = (scope) => {
+    let setCurrentLanguage;
+
+    scope.changeLanguage = (lang) => {
       $translate.use(lang);
     }
+
+    $rootScope.$on('$translateChangeEnd', (event, data) => {
+      scope.currentLanguage = data.language;
+    })
   }
 
   return {
     restrict: 'E',
     scope: {},
     template: require('./translateSwitcher.html'),
-    controller:  translateSwitchCtrl
+    link:  translateSwitchCtrl
   }
 }
