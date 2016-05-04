0fe4f45550050d197204de3f7ad1e89fdc7cc9da
Piotr Koson <pior.koson@akra.net>
2016-05-04 15:44:56 +0200

    adds new candidate module

--- client/app/routes/candidates/addNew/AddNewCtrl.js
+++ client/app/routes/candidates/addNew/AddNewCtrl.js
@@ -2,5 +2,19 @@ export default class AddNewCtrl {
   constructor($scope, $mdDialog, $mdMedia) {
     let vm = this;
     vm.$scope = $scope;
+    vm.$mdDialog = $mdDialog;
+    vm.languages = [];
+    vm.$scope.cancel = ()=> {
+      vm.$mdDialog.cancel();
+    };
+    function transformChip(chip) {
+      // If it is an object, it's already a known chip
+      if (angular.isObject(chip)) {
+        return chip;
+      }
+
+      // Otherwise, create a new one
+      return { name: vm.chip, type: 'new' };
+    }
   }
 }
