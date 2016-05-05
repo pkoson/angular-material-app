bd81a27c0696846c5c8e0040884cc95025eaddc5
Tomasz Kulik <Tomasz.Kulik@akra.net>
2016-05-05 10:16:27 +0200

    rubocop fix ofenses

--- app/controllers/api/v1/locales_controller.rb
+++ app/controllers/api/v1/locales_controller.rb
@@ -2,6 +2,6 @@
 class Api::V1::LocalesController < ApplicationController
   def index
     yml = YAML.load_file("config/locales/front.#{I18n.locale}.yml")
-    render json: yml["#{I18n.locale}"].to_json
+    render json: yml[I18n.locale.to_s].to_json
   end
 end
