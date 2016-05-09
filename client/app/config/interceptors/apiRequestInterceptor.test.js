6a1324e07f5ce4466884e1537bebdadd285092b1
Bartlomiej Slipek <bslipek@gmail.com>
2016-05-09 14:51:09 +0200

    Changes API url depends of host

--- client/app/config/interceptors/apiRequestInterceptor.test.js
+++ client/app/config/interceptors/apiRequestInterceptor.test.js
@@ -2,6 +2,7 @@ import apiRequestInterceptor from './apiRequestInterceptor';
 
 describe('apiRequestInterceptor', () => {
 
+  let mocks = {};
   let fakedData = {};
   let interceptor, $rootScope, $httpProvider;
 
@@ -16,23 +17,52 @@ describe('apiRequestInterceptor', () => {
         .config(function($httpProvider) {
           $httpProvider.interceptors.push('apiRequestInterceptor');
         });
-    
+
     angular.mock.module('mock.app.config', (_$httpProvider_) => {
       $httpProvider = _$httpProvider_;
     });
-  
-    window.inject((_apiRequestInterceptor_) => {
-      interceptor = _apiRequestInterceptor_;
-    }); 
+
+    mocks.$location = {
+      host: jasmine.createSpy()
+    };
+
+    window.module($provide => {
+      $provide.value('$location', mocks.$location);
+    });
   });
 
 
+  describe('localhost server', () => {
 
-  it('should add correct prefix to request', () => {
-    let config = angular.copy(fakedData.config);
-    let request = interceptor.request(config);
+    beforeEach(() => {
+      mocks.$location.host.and.returnValue('localhost');
+      window.inject((_apiRequestInterceptor_) => {
+        interceptor = _apiRequestInterceptor_;
+      });
+    });
 
-    expect(request.url).toEqual('http://lvh.me:3000/api/v1' + fakedData.config.url);
+    it('should add correct prefix to request', () => {
+      let config = angular.copy(fakedData.config);
+      let request = interceptor.request(config);
+
+      expect(request.url).toEqual('http://lvh.me:3000/api/v1' + fakedData.config.url);
+    });
   });
 
+  describe('sportmatrix.dev.akra.net server', () => {
+
+    beforeEach(() => {
+      mocks.$location.host.and.returnValue('sportmatrix');
+      window.inject((_apiRequestInterceptor_) => {
+        interceptor = _apiRequestInterceptor_;
+      });
+    });
+
+    it('should add correct prefix to request', () => {
+      let config = angular.copy(fakedData.config);
+      let request = interceptor.request(config);
+
+      expect(request.url).toEqual('http://sportmatrix.dev.akra.net/api/v1' + fakedData.config.url);
+    });
+  });
 });
