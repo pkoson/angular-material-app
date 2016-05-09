b1d5e1ef9013b96a7e80a1f4dfc9a93e469c0958
Jerzy Daszkowski <jerzy.daszkowski@akra.net>
2016-05-09 14:47:29 +0200

    Change directive to component - before pull request [#117892207]

--- client/app/components/translateSwitcher/index.js
+++ client/app/components/translateSwitcher/index.js
@@ -3,5 +3,5 @@ import angular from 'angular';
 import translateSwitcher from './translateSwitcher.directive';
 
 export default angular.module('app.components.translateSwitcher', [])
-  .directive('translateSwitcher', translateSwitcher)
+  .component('translateSwitcher', translateSwitcher)
   .name;
--- client/app/components/translateSwitcher/translateSwitcher.directive.js
+++ client/app/components/translateSwitcher/translateSwitcher.directive.js
@@ -1,20 +1,27 @@
-export default function translateSwitcher($translate, $rootScope) {
-  "ngInject";
-  let translateSwitchCtrl = (scope) => {
+class translateSwitcherCtrl {
+  constructor($scope, $translate, $rootScope) {
+    "ngInject";
+    this.$translate = $translate;
+    this.$rootScope = $rootScope;
+    this.$scope = $scope;
 
-    scope.changeLanguage = (lang) => {
-      $translate.use(lang);
-    };
+    this.addListerer();
+  }
 
-    $rootScope.$on('$translateChangeEnd', (event, data) => {
-      scope.currentLanguage = data.language;
+  changeLanguage (lang) {
+    this.$translate.use(lang);
+  }
+
+  addListerer (){
+    this.$rootScope.$on('$translateChangeEnd', (event, data) => {
+      this.$scope.currentLanguage = data.language;
     });
-  };
-
-  return {
-    restrict: 'E',
-    scope: {},
-    template: require('./translateSwitcher.html'),
-    link:  translateSwitchCtrl
-  };
+  }
 }
+
+let translateSwitcher = {
+  template: require('./translateSwitcher.html'),
+  controller: translateSwitcherCtrl,
+};
+
+export default translateSwitcher;
